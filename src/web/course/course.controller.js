c99fc24acaa6ec129267ede14a835c59ef2c90ec
Aditya <adityadhar.hipl@gmail.com>
2026-06-16 17:36:07 +0530

    done

--- src/web/course/course.controller.js
+++ src/web/course/course.controller.js
@@ -1,27 +1,6 @@
-// web/course/course.controller.js
 
 
 const Course = require('./course.model');
-
-// exports.getCourses = async (req, res) => {
-//   try {
-//     const courses = await Course.find().sort({
-//       createdAt: -1,
-//     });
-
-//     res.status(200).json({
-//       success: true,
-//       count: courses.length,
-//       data: courses,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
-
 exports.getCourses = async (req, res) => {
   try {
     const courses = await Course.find()
